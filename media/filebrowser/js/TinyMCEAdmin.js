16477a3a2cb6736e8ad1ec54ece972c60d509af6
Patrick Kranzlmueller <patrick@vonautomatisch.at>
2008-08-27 10:46:59 +0000

    tinymce admin setup update

--- media/filebrowser/js/TinyMCEAdmin.js
+++ media/filebrowser/js/TinyMCEAdmin.js
@@ -19,33 +19,58 @@ function CustomFileBrowser(field_name, url, type, win) {
     return false;
 }
 
-function myCustomSetupContent(editor_id, body, doc) {
-    if (body.innerHTML == "") {
-        body.innerHTML = "<p>xxx</p>";
-    }
-}
 
 tinyMCE.init({
     mode: "textareas",
-    theme : "advanced",
-    language : "de",
-    theme_advanced_toolbar_location : "top",
-    theme_advanced_toolbar_align : "left",
-    theme_advanced_statusbar_location : "",
-    theme_advanced_buttons1 : "formatselect,bold,italic,underline,bullist,numlist,undo,redo,link,unlink,image,code,fullscreen",
-    theme_advanced_buttons2 : "",
-    theme_advanced_buttons3 : "",
-    theme_advanced_path : false,
-    theme_advanced_blockformats : "p,h2,h3",
-    width: '500',
+    theme: "advanced",
+    language: "en",
+    skin: "o2k7",
+    browsers: "gecko",
+    dialog_type: "modal",
+    object_resizing: true,
+    cleanup_on_startup: true,
+    forced_root_block: "p",
+    remove_trailing_nbsp: true,
+    theme_advanced_toolbar_location: "top",
+    theme_advanced_toolbar_align: "left",
+    theme_advanced_statusbar_location: "none",
+    theme_advanced_buttons1: "formatselect,styleselect,bold,italic,underline,bullist,numlist,undo,redo,link,unlink,image,code,template,visualchars,fullscreen,pasteword,media,search,replace,charmap",
+    theme_advanced_buttons2: "",
+    theme_advanced_buttons3: "",
+    theme_advanced_path: false,
+    theme_advanced_blockformats: "p,h2,h3,h4,div,code,pre",
+    theme_advanced_styles: "[all] clearfix=clearfix;[p] summary=summary;[div] code=code;[img] img_left=img_left;[img] img_left_nospacetop=img_left_nospacetop;[img] img_right=img_right;[img] img_right_nospacetop=img_right_nospacetop;[img] img_block=img_block;[img] img_block_nospacetop=img_block_nospacetop;[div] column span-2=column span-2;[div] column span-4=column span-4;[div] column span-8=column span-8",
+    width: '700',
     height: '200',
-    editor_css : "/media/css/tinymce/ui_admin.css",
-    content_css : "/media/css/tinymce/content_admin.css",
-    popups_css : "/media/css/tinymce/popup_admin.css",
-    plugins : "advimage,advlink,fullscreen",
-    advimage_styles : "Linksbündig neben Text=img_left;Rechtsbündig neben Text=img_right;Eigener Block=img_block",
-    advlink_styles : "intern (innerhalb von skipclass.at)=internal;extern (Link zu einer externen Seite)=external",
+    plugins: "advimage,advlink,fullscreen,visualchars,paste,media,template,searchreplace",
+    advimage_styles: "Linksbündig neben Text=img_left;Rechtsbündig neben Text=img_right;Eigener Block=img_block",
+    advlink_styles: "internal (sehmaschine.net)=internal;external (link to an external site)=external",
     advimage_update_dimensions_onchange: true,
-    file_browser_callback : "CustomFileBrowser",
-    relative_urls : false
+    file_browser_callback: "CustomFileBrowser",
+    relative_urls: false,
+    valid_elements : "" +
+    "-p," + 
+    "a[href|target=_blank|class]," +
+    "-strong/-b," +
+    "-em/-i," +
+    "-u," + 
+    "-ol," + 
+    "-ul," + 
+    "-li," + 
+    "br," + 
+    "img[class|src|alt=|width|height]," + 
+    "-h2,-h3,-h4," + 
+    "-pre," +
+    "-code," + 
+    "-div",
+    extended_valid_elements: "" + 
+    "a[name|class|href|target|title|onclick]," + 
+    "img[class|src|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name]," + 
+    "br[clearfix]," + 
+    "-p[class<clearfix?summary?code]," + 
+    "h2[class<clearfix],h3[class<clearfix],h4[class<clearfix]," + 
+    "ul[class<clearfix],ol[class<clearfix]," + 
+    "div[class],",
 });
+
+
