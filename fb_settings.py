5fe7f68f898d5dba50bd3e5e53f5959c3661f41a
Patrick Kranzlmueller <patrick@vonautomatisch.at>
2008-09-05 14:58:09 +0000

    updated comments in fb_settings.py

--- fb_settings.py
+++ fb_settings.py
@@ -1,23 +1,36 @@
 import os
 from django.conf import settings
 
-# your media-url
+# The location of your media-files.
+# This is the base URL for all your media-files accessible for the FileBrowser.
+# Note: When you set URL_WWW to settings.MEDIA_URL you can use all your media-files with the FileBrowser.
+# Nevertheless, you may want to limit this to a subdirectory of settings.MEDIA_URL or a seperate media-server.
+# Important: If you change this setting, you should also change PATH_SERVER.
 URL_WWW = getattr(settings, "FILEBROWSER_URL_WWW", settings.MEDIA_URL + 'uploads/')
-# your filebrowser admin url
+
+# The FileBrowser Admin-URL.
+# Note: If you change this URL, you also have to change the file urls.py.
 URL_ADMIN = getattr(settings, "FILEBROWSER_URL_ADMIN", '/admin/filebrowser/')
-# home url
+
+# The URL to your Admin Index-Site.
 URL_HOME = getattr(settings, "FILEBROWSER_URL_HOME", '/admin/')
 
-# paths
+# The server-path to media-files. This is the initial/root server-path for the FileBrowser.
+# Important: If you change this setting, you should also change URL_WWW.
 PATH_SERVER = getattr(settings, "FILEBROWSER_PATH_SERVER", os.path.join(settings.MEDIA_ROOT, 'uploads'))
-# path to your filebrowser media (img/js/css)
+
+# The server-path to your filebrowser media-files.
+# Note: You have to change this setting, if you install the media-files of the FileBrowser outside
+# your admin-media directory. 
 PATH_FILEBROWSER_MEDIA = getattr(settings, "FILEBROWSER_PATH_MEDIA", "/media/admin/filebrowser/")
-# path to tinymce
+
+# The server-path to your TinyMCE Installation.
+# Note: You have to change this setting, if you install TinyMCE outside your admin-media directory.
 PATH_TINYMCE = getattr(settings, "FILEBROWSER_PATH_TINYMCE", "/media/admin/tinymce/jscripts/tiny_mce/")
 
-# extensions / lower case (important)
-# note that there are icons based on the extension-categories (Image, Video, ...). 
-# therefore, if you add a category (e.g. "Misc"), you won't get an icon.
+# Allowed Extensions for File Upload. Lower case is important.
+# Please be aware that there are Icons for the default extension settings.
+# Therefore, if you add a category (e.g. "Misc"), you won't get an icon.
 EXTENSIONS = getattr(settings, "FILEBROWSER_EXTENSIONS", {
     'Folder':[''],
     'Image':['.jpg', '.jpeg', '.gif','.png','.tif','.tiff'],
@@ -27,27 +40,36 @@ EXTENSIONS = getattr(settings, "FILEBROWSER_EXTENSIONS", {
     'Code':['.html','.py','.js','.css']
 })
 
-# max upload size in bytes
+# Max. Upload Size in Bytes.
 MAX_UPLOAD_SIZE = getattr(settings, "FILEBROWSER_MAX_UPLOAD_SIZE", 5000000)
 
-# thumbnail-prefix / thumbnail size
+# The prefix for your thumbnails.
+# If you have an Image "myimage.jpg", your thumbnail will be "thumb_myimage.jpg" by default.
 THUMB_PREFIX = getattr(settings, 'FILEBROWSER_THUMB_PREFIX', 'thumb_')
+
+# The size of your thumbnails for the Admin-Interface.
+# Note: This Thumbnail is for diplaying your Image within the Admin-Interface.
+# Because of the low quality, it´s not intended to use this Thumbnail on your Website.
+# For displaying Thumbnails on a Website, use "Image Generator" instead.
 THUMBNAIL_SIZE = getattr(settings, 'FILEBROWSER_THUMBNAIL_SIZE', (50, 150))
 
-# use image generator
+# Whether or not to use the ImageGenerator.
+# When this is True, you´ll get a checkbox called "Use Image Generator" with every Upload-Field.
+# Moreover, every Image will have a button "Generate Images" to generate Image versions
+# (this is useful, if Images are uploaded using FTP and not the FileBrowser -
+# you can upload Images using FTP and generate the Image Versions afterwards). 
 USE_IMAGE_GENERATOR = getattr(settings, 'FILEBROWSER_USE_IMAGE_GENERATOR', True)
-# image generator directory
-# the name of the directory (which contains the generated image vesions) is constructed like this:
-# image_name with dots replaced with underlines + IMAGE_GENERATOR_DIRECTORY
-# example: an image named "my_holiday_image_1.jpg" will have a versions directory
-# named "my_holiday_image_1_jpg_versions"
+
+# The postfix for your "Image Versions"-directory.
+# If you Upload an Image called "myimage.jpg", your versions-directory will be called
+# "myimage_jpg_versions" by default. 
 IMAGE_GENERATOR_DIRECTORY = getattr(settings, 'FILEBROWSER_IMAGE_GENERATOR_DIRECTORY', '_versions')
-# image generator (prefix, new image width)
-# if you do not need to save different image sizes
-# write IMAGE_GENERATOR_LANDSCAPE = "" and IMAGE_GENERATOR_PORTRAIT = ""
+
+# A list of Images to generate in the format (prefix, image width).
 IMAGE_GENERATOR_LANDSCAPE = getattr(settings, "FILEBROWSER_IMAGE_GENERATOR_LANDSCAPE", [('thumbnail_',140),('small_',300),('medium_',460),('big_',620)])
+
+# A list of Images to generate in the format (prefix, image width).
 IMAGE_GENERATOR_PORTRAIT = getattr(settings, "FILEBROWSER_IMAGE_GENERATOR_PORTRAIT", [('thumbnail_',140),('small_',300),('medium_',460),('big_',620)])
-# generator for a cropped image (prefix, width)
-# if you do not need to save different image sizes
-# write IMAGE_GENERATOR = ""
+
+# A list of Images to generate in the format (prefix, image width, image height). 
 IMAGE_CROP_GENERATOR = getattr(settings, "FILEBROWSER_IMAGE_CROP_GENERATOR", [('cropped_',60,60),('croppedthumbnail_',140,140)])
