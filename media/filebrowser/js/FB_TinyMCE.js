4f4f75632e6887b1ffdb24d7de452cbb48a851b7
Patrick Kranzlmueller <patrick@vonautomatisch.at>
2008-11-18 14:38:40 +0000

    solved ticket #46

--- media/filebrowser/js/FB_TinyMCE.js
+++ media/filebrowser/js/FB_TinyMCE.js
@@ -12,10 +12,12 @@ var FileBrowserDialogue = {
         win.document.getElementById(tinyMCEPopup.getWindowArg("input")).value = URL;
         
         // change width/height & show preview
-        if (win.ImageDialog.getImageData)
-            win.ImageDialog.getImageData();
-        if (win.ImageDialog.showPreviewImage)
-            win.ImageDialog.showPreviewImage(URL);
+        if (win.ImageDialog){
+            if (win.ImageDialog.getImageData)
+                win.ImageDialog.getImageData();
+            if (win.ImageDialog.showPreviewImage)
+                win.ImageDialog.showPreviewImage(URL);
+        }
         
         // close popup window
         tinyMCEPopup.close();
