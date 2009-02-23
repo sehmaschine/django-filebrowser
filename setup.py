0e75c3500faad3d6bb79250e1d737acc94a46629
Patrick Kranzlmueller <patrick@vonautomatisch.at>
2009-02-23 16:55:07 +0000

    distutils compatibility: added setup-file

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-filebrowser',
+    version='2.1.0',
+    description='File-Management with the Django Admin-Interface.',
+    author='Patrick Kranzlmueller',
+    author_email='patrick@vonautomatisch.at',
+    url='http://code.google.com/p/django-filebrowser/',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ]
+)
