da235eb30e4754f0aa268df27a59a51e0aef530c
Patrick Kranzlmueller <patrick@vonautomatisch.at>
2009-05-03 14:09:59 +0000

    small update to fb_setttings

--- fb_settings.py
+++ fb_settings.py
@@ -10,8 +10,8 @@ try:
     DEFAULT_URL_TINYMCE = tinymce.settings.JS_BASE_URL + '/'
     DEFAULT_PATH_TINYMCE = tinymce.settings.JS_ROOT + '/'
 except ImportError:
-    DEFAULT_URL_TINYMCE = settings.ADMIN_MEDIA_PREFIX + "tinymce_2/jscripts/tiny_mce/"
-    DEFAULT_PATH_TINYMCE = os.path.join(settings.MEDIA_ROOT, 'admin/tinymce_2/jscripts/tiny_mce/')
+    DEFAULT_URL_TINYMCE = settings.ADMIN_MEDIA_PREFIX + "tinymce/jscripts/tiny_mce/"
+    DEFAULT_PATH_TINYMCE = os.path.join(settings.MEDIA_ROOT, 'admin/tinymce/jscripts/tiny_mce/')
 
 # The location of your media-files.
 # This is the base URL for all your media-files accessible for the FileBrowser.
