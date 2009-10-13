a457108da15a4e311e83c6272fd352ca81723494
Patrick Kranzlmueller <patrick@vonautomatisch.at>
2009-10-13 10:22:05 +0000

    updated setup, resolved issue#199

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='django-filebrowser',
     version='3.0',
-    description='File-Management with the Django Admin-Interface.',
+    description='Media-Management with the Django Admin-Interface.',
     author='Patrick Kranzlmueller',
     author_email='patrick@vonautomatisch.at',
     url='http://code.google.com/p/django-filebrowser/',
@@ -11,10 +11,10 @@ setup(
     package_data = {'filebrowser': [
         'templates/filebrowser/*.html',
         'templates/filebrowser/include/*',
-        'media/filebrowser/css/*',
-        'media/filebrowser/img/*',
-        'media/filebrowser/js/*',
-        'locale/*/LC_MESSAGES/*']},
+        'templatetags/*',
+        'locale/*/LC_MESSAGES/*',
+        'media/*/*/*']},
+    zip_safe=False,
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
