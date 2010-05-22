ba256e73fbbb26707222e81b18b59d141a0890f0
Patrick Kranzlmueller <patrick@vonautomatisch.at>
2010-05-22 18:08:10 +0000

    initializing filebrowser 3.1

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-filebrowser',
+    version='3.1',
+    description='Media-Management with the Django Admin-Interface.',
+    author='Patrick Kranzlmueller',
+    author_email='patrick@vonautomatisch.at',
+    url='http://code.google.com/p/django-filebrowser/',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ]
+)
