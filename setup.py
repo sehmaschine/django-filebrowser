756abdb27de26c3947400d01560323762ba5e922
Matjaz Crnko <matjaz.crnko@gmail.com>
2010-11-19 15:19:36 +0000

    Added setup.py, for real this time.

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-filebrowser-no-grappelli',
+    version='3.0',
+    description='Media-Management with the Django Admin-Interface. Without the grappelli dependency',
+    author='Patrick Kranzlmueller',
+    author_email='patrick@vonautomatisch.at',
+    contributors=[
+        ['Matjaž Črnko', 'matjaz.crnko@gmail.com', 'no-grappelli work'],
+    ]
+    url='http://code.google.com/p/django-filebrowser/',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ]
+)
