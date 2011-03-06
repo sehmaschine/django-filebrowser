747ed3cacc89af3d9e28992ed8eb4c1e9c3107c5
Patrick Kranzlmueller <patrick@vonautomatisch.at>
2011-03-06 22:10:04 +0000

    updated license, manifest, setup and version

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-filebrowser',
+    version='3.3.0',
+    description='Media-Management with the Django Admin-Interface.',
+    author='Patrick Kranzlmueller (vonautomatisch)',
+    author_email='werkstaetten@vonautomatisch.at',
+    url='http://code.google.com/p/django-filebrowser/',
+    download_url='',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ]
+)
