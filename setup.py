9b206b9c303ba49399a6ce53489e303f4b3f7457
Patrick Kranzlmueller <patrick@vonautomatisch.at>
2011-03-10 14:53:46 +0000

    trunk = 3.2

--- setup.py
+++ setup.py
@@ -2,17 +2,19 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-filebrowser',
-    version='development',
+    version='3.2',
     description='Media-Management with the Django Admin-Interface.',
-    author='Patrick Kranzlmueller',
-    author_email='patrick@vonautomatisch.at',
+    author='Patrick Kranzlmueller (vonautomatisch)',
+    author_email='werkstaetten@vonautomatisch.at',
     url='http://code.google.com/p/django-filebrowser/',
+    download_url='',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
+        'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
