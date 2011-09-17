41ae1ac475764839144a7c62b3bc85d42727a8fe
sehmaschine <patrick@vonautomatisch.at>
2011-09-17 15:18:13 +0200

    initial implementation of an ajax-based uploader
    replaces previous flash-based solution with uploadify

--- filebrowser/decorators.py
+++ filebrowser/decorators.py
@@ -17,26 +17,6 @@ from filebrowser.functions import get_path, get_file
 from filebrowser.templatetags.fb_tags import query_helper
 
 
-def flash_login_required(function):
-    """
-    Decorator to recognize a user by its session.
-    Used for Flash-Uploading.
-    """
-    
-    def decorator(request, *args, **kwargs):
-        try:
-            engine = __import__(settings.SESSION_ENGINE, {}, {}, [''])
-        except:
-            import django.contrib.sessions.backends.db
-            engine = django.contrib.sessions.backends.db
-        session_data = engine.SessionStore(request.POST.get('session_key'))
-        user_id = session_data['_auth_user_id']
-        # will return 404 if the session ID does not resolve to a valid user
-        request.user = get_object_or_404(User, pk=user_id)
-        return function(request, *args, **kwargs)
-    return decorator
-
-
 def path_exists(site, function):
     """
     Check if the given path exists.
