90dd79328803f7af2f91be2948d985feb6798512
vam <vaclav.mikolasek@gmail.com>
2011-10-02 22:20:18 +0200

    isdir and isfile were too inefficient - made abstract and storage-specific.

--- filebrowser/storage.py
+++ filebrowser/storage.py
@@ -10,21 +10,17 @@ class StorageMixin(object):
     Adds some useful methods to the Storage class.
     """
 
-    def isdir(self, path):
-        dirname, basename = os.path.split(path)
-        if not basename: # path ends with a slash (probably a directory, but let's be sure..)
-            dirname = os.path.dirname(dirname)
-            basename = os.path.relpath(path, dirname) # Gets rid of the trailing slash
-        dirs, files = self.listdir(dirname)
-        return basename in dirs
+    def isdir(self, name):
+        """
+        Returns true if name exists and is a directory.
+        """
+        raise NotImplementedError()
 
-    def isfile(self, path):
-        dirname, basename = os.path.split(path)
-        if not basename: # path ends with a slash (probably a directory, but let's be sure..)
-            dirname = os.path.dirname(dirname)
-            basename = os.path.relpath(path, dirname) # Gets rid of the trailing slash
-        dirs, files = self.listdir(dirname)
-        return basename in files
+    def isfile(self, name):
+        """
+        Returns true if name exists and is a regular file.
+        """
+        raise NotImplementedError()
 
     def move(self, old_file_name, new_file_name, allow_overwrite=False):
         """
@@ -33,31 +29,6 @@ class StorageMixin(object):
         If allow_ovewrite==False and old_file_name exists, raises an exception.
         """
         raise NotImplementedError()
-        # tmpfile = None
-        # if self.exists(new_file_name):
-        #     if not allow_overwrite:
-        #         raise 'File already exists. Try: allow_overwrite=True'
-        #     else:
-        #         # Make a temporary copy of the file that gets overwritten
-        #         try:
-        #             tmpfile = self.open(new_file_name)
-        #             tmpfile_name = self.save(new_file_name, tmpfile)
-        #         finally:
-        #             tmpfile.close()
-        #         # Delete the original file
-        #         self.delete(new_file_name)                
-        # # Copy the old file to the new location and delete the old one
-        # try:
-        #     old_file = self.open(old_file_name)
-        #     self.save(new_file_name, old_file)
-        # finally:
-        #     old_file.close()
-        # # Delete the old file
-        # self.delete(old_file_name)
-        # # Delete tmpfile if created
-        # if tmpfile:
-        #     self.delete(tmpfile_name)
-
 
     def rename(self, old_file_name, new_file_name):
         """
@@ -70,14 +41,6 @@ class StorageMixin(object):
         Creates all missing directories specified by name. Analogue to os.mkdirs().
         """
         raise NotImplementedError()
-        # if self.isfile(name):
-        #     raise IOError('%s exists and is a file.' % name)
-        # f = ContentFile('') # empty file
-        # # We assume that self.save() creates dirs as necessary
-        # filename = self.save(os.path.join(name, 'tmp'), f)
-        # self.delete(os.path.join(name, 'tmp'))
-        # if not self.exists(name):
-        #     raise IOError('Failed to create dir %s' % name)
 
     def rmtree(self, name):
         """
@@ -86,6 +49,12 @@ class StorageMixin(object):
         raise NotImplementedError()
 
 class FileSystemStorageMixin(StorageMixin):
+
+    def isdir(self, name):
+        return os.path.isdir(self.path(name))
+
+    def isfile(self, name):
+        return os.path.isfile(self.path(name))
 
     def move(self, old_file_name, new_file_name, allow_overwrite=False):
         file_move_safe(self.path(old_file_name), self.path(new_file_name), allow_overwrite=True)
