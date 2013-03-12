817bcc3f38a7ce45ea4b6728adaa56483f63de7a
Maxime Haineault <max@motion-m.ca>
2013-03-12 11:00:13 -0400

    Changed exception handling syntax to be backward compatible with Python 2.5

--- filebrowser/templatetags/fb_versions.py
+++ filebrowser/templatetags/fb_versions.py
@@ -124,7 +124,7 @@ class VersionObjectNode(Node):
             elif site.storage.modified_time(source) > site.storage.modified_time(version_path):
                 version_path = version_generator(source, version_prefix, force=True, site=site)
             context[self.var_name] = FileObject(version_path, site=site)
-        except Exception as e:
+        except Exception, e:
             if settings.TEMPLATE_DEBUG:
                 raise e
             context[self.var_name] = ""
