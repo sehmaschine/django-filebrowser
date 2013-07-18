25076d3e7fd8c4fa9a72ea91631ea6d3a42ffdf2
Patrick Kranzlmueller <sehmaschine@gmail.com>
2013-07-18 12:28:30 +0200

    new utils (instead of functions), #167

---
+++ filebrowser/utils.py
@@ -0,0 +1,65 @@
+# coding: utf-8
+
+# PYTHON IMPORTS
+import os
+
+# FILEBROWSER IMPORTS
+from filebrowser.settings import STRICT_PIL
+
+# PIL import
+if STRICT_PIL:
+    from PIL import Image
+else:
+    try:
+        from PIL import Image
+    except ImportError:
+        import Image
+
+
+def path_strip(path, root):
+    if not path or not root:
+        return path
+    path = os.path.normcase(path)
+    root = os.path.normcase(root)
+    if path.startswith(root):
+        return path[len(root):]
+    return path
+
+
+def scale_and_crop(im, width, height, opts):
+    """
+    Scale and Crop.
+    """
+    
+    x, y   = [float(v) for v in im.size]
+    
+    if 'upscale' not in opts and x < width:
+        # version would be bigger than original
+        # no need to create this version, because "upscale" isn't defined.
+        return False
+    
+    if width:
+        xr = float(width)
+    else:
+        xr = float(x*height/y)
+    if height:
+        yr = float(height)
+    else:
+        yr = float(y*width/x)
+    
+    if 'crop' in opts:
+        r = max(xr/x, yr/y)
+    else:
+        r = min(xr/x, yr/y)
+    
+    if r < 1.0 or (r > 1.0 and 'upscale' in opts):
+        im = im.resize((int(x*r), int(y*r)), resample=Image.ANTIALIAS)
+    
+    if 'crop' in opts:
+        x, y   = [float(v) for v in im.size]
+        ex, ey = (x-min(x, xr))/2, (y-min(y, yr))/2
+        if ex or ey:
+            im = im.crop((int(ex), int(ey), int(x-ex), int(y-ey)))
+    return im
+    
+scale_and_crop.valid_options = ('crop', 'upscale')
