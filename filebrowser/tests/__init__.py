f8d610b3c91f318170fdffc789eab2d55c77382d
Patrick Kranzlmueller <sehmaschine@gmail.com>
2013-12-01 17:00:59 +0100

    test compatibility with django 1.6

--- filebrowser/tests/__init__.py
+++ filebrowser/tests/__init__.py
@@ -1,9 +1,9 @@
-from filebrowser.tests.settings import SettingsTests
-from filebrowser.tests.base import FileObjectPathTests, FileObjectUnicodeTests
-from filebrowser.tests.sites import *
+from filebrowser.tests.test_settings import SettingsTests
+from filebrowser.tests.test_base import FileObjectPathTests, FileObjectUnicodeTests
+from filebrowser.tests.test_sites import *
 
 # These tests will create directories and files within MEDIA_ROOT
 # Only use this with your local environment
-from filebrowser.tests.base import FileObjectAttributeTests, FileListingTests
-from filebrowser.tests.versions import VersionTemplateTagsTests
-from filebrowser.tests.commands import CommandsTests
+from filebrowser.tests.test_base import FileObjectAttributeTests, FileListingTests
+from filebrowser.tests.test_versions import VersionTemplateTagsTests
+from filebrowser.tests.test_commands import CommandsTests
