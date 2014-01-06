4fb51456d58e2f86679c2a64421bb3f59d5a20d9
Patrick Kranzlmueller <sehmaschine@gmail.com>
2014-01-06 17:20:11 +0100

    changed docs configuration in order to use the new sphinx theme correctly

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,8 @@
 # serve to show the default.
 
 import sys, os
-import sphinx_rtd_theme
+
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -92,8 +93,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#html_theme = 'default'
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -102,7 +102,6 @@ html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = ["_theme"]
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
@@ -218,3 +217,8 @@ man_pages = [
     ('index', 'djangofilebrowser', u'Django FileBrowser Documentation',
      [u'Patrick Kranzlmueller'], 1)
 ]
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
