4b9dac3f5c2497a419e10dd91d8c4982003506d4
Adam Patterson <adam@adamrt.com>
2015-09-23 18:19:31 -0500

    Reduce complexity of setUp and tearDown methods

--- tests/test_versions.py
+++ tests/test_versions.py
@@ -38,77 +38,16 @@ F_MISSING = FileObject(os.path.join(fb_settings.DIRECTORY, 'filebrowser_test', "
 
 class ScaleAndCropTests(TestCase):
     def setUp(self):
-        """
-        Save original values/functions so they can be restored in tearDown
-        """
-        self.original_path = filebrowser.base.os.path
-        self.original_directory = site.directory
-        self.original_versions_basedir = filebrowser.base.VERSIONS_BASEDIR
-        self.original_versions = filebrowser.base.VERSIONS
-        self.original_admin_versions = filebrowser.base.ADMIN_VERSIONS
-        self.original_placeholder = fb_versions.PLACEHOLDER
-        self.original_show_placeholder = fb_versions.SHOW_PLACEHOLDER
-        self.original_force_placeholder = fb_versions.FORCE_PLACEHOLDER
-
-        # DIRECTORY
-        # custom directory because this could be set with sites
-        # and we cannot rely on filebrowser.settings
-        # FIXME: find better directory name
-        self.directory = "fb_test_directory/"
-        self.directory_path = os.path.join(site.storage.location, self.directory)
-        if os.path.exists(self.directory_path):
-            self.fail("Test directory already exists.")
-        else:
-            os.makedirs(self.directory_path)
-        # set site directory
-        site.directory = self.directory
-
-        # VERSIONS
-        self.versions = "_versionstestdirectory"
-        self.versions_path = os.path.join(site.storage.location, self.versions)
-        if os.path.exists(self.versions_path):
-            self.fail("Versions directory already exists.")
-        else:
-            os.makedirs(self.versions_path)
-
-        # create temporary test folder and move testimage
-        # FIXME: find better path names
-        self.tmpdir_name = os.path.join("fb_tmp_dir", "fb_tmp_dir_sub")
-        self.tmpdir_path = os.path.join(site.storage.location, self.directory, self.tmpdir_name)
-        if os.path.exists(self.tmpdir_path):
-            self.fail("Temporary testfolder already exists.")
-        else:
-            os.makedirs(self.tmpdir_path)
-
-        # copy test image to temporary test folder
-        self.image_path = os.path.join(FILEBROWSER_PATH, "static", "filebrowser", "img", "testimage.jpg")
-        if not os.path.exists(self.image_path):
-            self.fail("Testimage not found.")
-        shutil.copy(self.image_path, self.tmpdir_path)
-
-        # create temporary test folder (placeholder) and move testimage
-        # FIXME: find better path names
-        self.tmpdir_name_ph = os.path.join("fb_tmp_dir", "fb_tmp_placeholder")
-        self.tmpdir_path_ph = os.path.join(site.storage.location, self.directory, self.tmpdir_name_ph)
-        if os.path.exists(self.tmpdir_path_ph):
-            self.fail("Temporary testfolder (placeholder) already exists.")
-        else:
-            os.makedirs(self.tmpdir_path_ph)
-
-        # copy test image to temporary test folder (placeholder)
-        shutil.copy(self.image_path, self.tmpdir_path_ph)
-
-        # set posixpath
-        filebrowser.base.os.path = posixpath
-
-        # fileobjects
-        self.f_image = FileObject(os.path.join(self.directory, self.tmpdir_name, "testimage.jpg"), site=site)
-        self.f_image_not_exists = FileObject(os.path.join(self.directory, self.tmpdir_name, "testimage_does_not_exist.jpg"), site=site)
-        self.f_folder = FileObject(os.path.join(self.directory, self.tmpdir_name), site=site)
-        self.f_placeholder = FileObject(os.path.join(self.directory, self.tmpdir_name_ph, "testimage.jpg"), site=site)
+        os.makedirs(TEST_PATH)
+        os.makedirs(PLACEHOLDER_PATH)
+        shutil.copy(STATIC_IMG_PATH, TEST_PATH)
+        shutil.copy(STATIC_IMG_PATH, PLACEHOLDER_PATH)
 
         self.im = Image.open(F_IMG.path_full)
 
+    def tearDown(self):
+        shutil.rmtree(TEST_PATH)
+        shutil.rmtree(PLACEHOLDER_PATH)
 
     def test_scale_width(self):
         version = scale_and_crop(self.im, 500, "", "")
@@ -236,6 +175,17 @@ class VersionTemplateTagTests(TestCase):
     {% version path "large" %}
 
     """
+    def setUp(self):
+        os.makedirs(TEST_PATH)
+        os.makedirs(PLACEHOLDER_PATH)
+
+        shutil.copy(STATIC_IMG_PATH, TEST_PATH)
+        shutil.copy(STATIC_IMG_PATH, PLACEHOLDER_PATH)
+
+    def tearDown(self):
+        shutil.rmtree(TEST_PATH)
+        shutil.rmtree(PLACEHOLDER_PATH)
+
         self.assertRaises(TemplateSyntaxError, lambda: Template('{% load fb_versions %}{% version obj.path %}'))
         self.assertRaises(TemplateSyntaxError, lambda: Template('{% load fb_versions %}{% version %}'))
 
@@ -333,6 +283,17 @@ class VersionAsTemplateTagTests(TestCase):
 
     """
 
+    def setUp(self):
+        os.makedirs(TEST_PATH)
+        os.makedirs(PLACEHOLDER_PATH)
+
+        shutil.copy(STATIC_IMG_PATH, TEST_PATH)
+        shutil.copy(STATIC_IMG_PATH, PLACEHOLDER_PATH)
+
+    def tearDown(self):
+        shutil.rmtree(TEST_PATH)
+        shutil.rmtree(PLACEHOLDER_PATH)
+
         t = Template('{% load fb_versions %}{% version path "large" as version_large %}{{ version_large.url }}')
         c = Context({"obj": self.f_image, "path": "fb_test_directory/fb_tmp_dir/fb_tmp_dir_sub/testimage.jpg"})
         r = t.render(c)
@@ -424,6 +385,17 @@ class VersionObjectTemplateTagTests(TestCase):
 
     """
 
+    def setUp(self):
+        os.makedirs(TEST_PATH)
+        os.makedirs(PLACEHOLDER_PATH)
+
+        shutil.copy(STATIC_IMG_PATH, TEST_PATH)
+        shutil.copy(STATIC_IMG_PATH, PLACEHOLDER_PATH)
+
+    def tearDown(self):
+        shutil.rmtree(TEST_PATH)
+        shutil.rmtree(PLACEHOLDER_PATH)
+
         self.assertRaises(TemplateSyntaxError, lambda: Template('{% load fb_versions %}{% version_object obj.path %}'))
         self.assertRaises(TemplateSyntaxError, lambda: Template('{% load fb_versions %}{% version_object %}'))
         self.assertRaises(TemplateSyntaxError, lambda: Template('{% load fb_versions %}{% version_object obj.path "medium" %}'))
@@ -497,22 +469,3 @@ class VersionObjectTemplateTagTests(TestCase):
 
     def test_version_setting(self):
         pass
-
-    def tearDown(self):
-        """
-        Restore original values/functions
-        """
-        filebrowser.base.os.path = self.original_path
-        site.directory = self.original_directory
-        filebrowser.base.VERSIONS_BASEDIR = self.original_versions_basedir
-        filebrowser.base.VERSIONS = self.original_versions
-        filebrowser.settings.VERSIONS = self.original_versions
-        fb_versions.VERSIONS = self.original_versions
-        filebrowser.base.ADMIN_VERSIONS = self.original_admin_versions
-        fb_versions.PLACEHOLDER = self.original_placeholder
-        fb_versions.SHOW_PLACEHOLDER = self.original_show_placeholder
-        fb_versions.FORCE_PLACEHOLDER = self.original_force_placeholder
-
-        # remove temporary directory and test folder
-        shutil.rmtree(self.directory_path)
-        shutil.rmtree(self.versions_path)
