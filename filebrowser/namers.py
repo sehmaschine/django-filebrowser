d6c0992dc6152ee71a122d8541c87fea16e2f4f8
Mesaj <mesaj94@gmail.com>
2017-12-03 13:00:43 +0100

    namers.py use decode() in Django 2.0 instead force_text

--- filebrowser/namers.py
+++ filebrowser/namers.py
@@ -2,7 +2,9 @@ from __future__ import unicode_literals
 import re
 from django.utils import six
 from django.utils.module_loading import import_string
-from django.utils.text import force_text
+import django
+if django.VERSION <= (2, 0):
+    from django.utils.text import force_text
 
 from .settings import VERSIONS, VERSION_NAMER
 
@@ -34,11 +36,18 @@ class VersionNamer(object):
 class OptionsNamer(VersionNamer):
 
     def get_version_name(self):
-        name = "{root}_{options}{extension}".format(
-            root=force_text(self.file_object.filename_root),
-            options=self.options_as_string,
-            extension=self.file_object.extension,
-        )
+        if django.VERSION <= (2, 0):
+            name = "{root}_{options}{extension}".format(
+                root=force_text(self.file_object.filename_root),
+                options=self.options_as_string,
+                extension=self.file_object.extension,
+            )
+        else:
+            name = "{root}_{options}{extension}".format(
+                root=decode(self.file_object.filename_root),
+                options=self.options_as_string,
+                extension=self.file_object.extension,
+            )
         return name
 
     def get_original_name(self):
