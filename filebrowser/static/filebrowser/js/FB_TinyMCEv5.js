53779e5794fdfbcf2c75e4feeeb566ead037369a
Alan Hicks <ahicks@p-o.co.uk>
2020-04-16 14:01:13 +0100

    Support added for TinyMCE v5

---
+++ filebrowser/static/filebrowser/js/FB_TinyMCEv5.js
@@ -0,0 +1,11 @@
+/**
+ * Function to post the selected file back to TinyMCE
+ */
+var FileBrowserDialogue = {
+    fileSubmit : function (FileURL) {
+        window.parent.postMessage({
+            mceAction: 'FileSelected',
+            content: FileURL
+        }, '*');
+    }
+}
