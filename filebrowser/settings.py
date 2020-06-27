bc25bf6505575db5ea1b8649f126da7a9638fb77
MuslimBeibytuly <muslimbeibytuly@gmail.com>
2020-06-28 04:31:04 +0600

    Change ugettext_lazy to gettext_lazy due to deprecation
    django.utils.deprecation.RemovedInDjango40Warning: django.utils.translation.ugettext_lazy() is deprecated in favor of django.utils.translation.gettext_lazy().

--- filebrowser/settings.py
+++ filebrowser/settings.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 from django.conf import settings
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 # Main FileBrowser Directory. Relative to site.storage.location.
