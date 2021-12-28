769384c79f709d0d95b687c36d16f3ffa4f0f51b
Patrick Kranzlmueller <sehmaschine@gmail.com>
2021-12-28 14:25:23 +0100

    removed providing_args from signals, fixed #404

--- filebrowser/signals.py
+++ filebrowser/signals.py
@@ -2,40 +2,39 @@
 
 from django.dispatch import Signal
 
-
 # upload signals
 # path: Absolute server path to the file/folder
 # name: Name of the file/folder
 # site: Current FileBrowserSite instance
-filebrowser_pre_upload = Signal(providing_args=["path", "file", "site"])
-filebrowser_post_upload = Signal(providing_args=["path", "file", "site"])
+filebrowser_pre_upload = Signal()
+filebrowser_post_upload = Signal()
 
 # mkdir signals
 # path: Absolute server path to the file/folder
 # name: Name of the file/folder
 # site: Current FileBrowserSite instance
-filebrowser_pre_createdir = Signal(providing_args=["path", "name", "site"])
-filebrowser_post_createdir = Signal(providing_args=["path", "name", "site"])
+filebrowser_pre_createdir = Signal()
+filebrowser_post_createdir = Signal()
 
 # delete signals
 # path: Absolute server path to the file/folder
 # name: Name of the file/folder
 # site: Current FileBrowserSite instance
-filebrowser_pre_delete = Signal(providing_args=["path", "name", "site"])
-filebrowser_post_delete = Signal(providing_args=["path", "name", "site"])
+filebrowser_pre_delete = Signal()
+filebrowser_post_delete = Signal()
 
 # rename signals
 # path: Absolute server path to the file/folder
 # name: Name of the file/folder
 # site: Current FileBrowserSite instance
 # new_name: New name of the file/folder
-filebrowser_pre_rename = Signal(providing_args=["path", "name", "new_name", "site"])
-filebrowser_post_rename = Signal(providing_args=["path", "name", "new_name", "site"])
+filebrowser_pre_rename = Signal()
+filebrowser_post_rename = Signal()
 
 # action signals
 # action_name: Name of the custom action
 # fileobjects: A list of fileobjects the action will be applied to
 # site: Current FileBrowserSite instance
 # result: The response you defined with your custom action
-filebrowser_actions_pre_apply = Signal(providing_args=['action_name', 'fileobjects', 'site'])
-filebrowser_actions_post_apply = Signal(providing_args=['action_name', 'filebjects', 'result', 'site'])
+filebrowser_actions_pre_apply = Signal()
+filebrowser_actions_post_apply = Signal()
